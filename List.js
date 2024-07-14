951c907ac71602b188bb5efd767d4807dbe06bb0
Ayesha-sohail0012 <ayeshasohailchippa99@gmail.com>
2024-07-14 06:30:00 -0700

    first commit

---
+++ List.js
@@ -0,0 +1,22 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+let todoList = [];
+let condition = true;
+while (condition) {
+    let toDos = await inquirer.prompt([{
+            name: 'first',
+            type: 'input',
+            message: chalk.green('What you want to Add in your todo list?')
+        },
+        {
+            name: 'second',
+            type: 'confirm',
+            message: chalk.bgYellow('Would you like to add more in your todos?'),
+            default: 'true'
+        },
+    ]);
+    todoList.push(toDos.first);
+    condition = (toDos.second);
+    console.log(todoList);
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+"# Cli-Todo-list" 
